29b2daaeacf272c91d66d0aac92606e10e4f649d
Diango <diango.gavidia@thebridge.social>
2024-12-10 15:03:10 -0400

    fix: buy ticket request dto

--- tests/modules/raffle/mothers/BuyTicketRequestDtoMother.ts
+++ tests/modules/raffle/mothers/BuyTicketRequestDtoMother.ts
@@ -5,20 +5,16 @@ import { BuyTicketRequestDto } from "../../../../src/modules/raffle/application/
 
 export class BuyTicketRequestDtoMother {
 	static create(params?: Partial<BuyTicketRequestDto>): BuyTicketRequestDto {
-		const ticketPrice = +faker.finance.amount();
-		const paymentAmount = ticketPrice;
-
 		return {
 			paymentId: faker.string.uuid(),
 			userId: faker.string.uuid(),
 			ticketNumbers: [faker.number.int()],
-			ticketPrice,
 			raffleId: faker.string.uuid(),
 			reference: faker.finance.iban(),
 			name: faker.person.fullName(),
 			dni: faker.string.alphanumeric(),
 			phone: faker.phone.number(),
-			paymentAmount,
+			paymentAmount: +faker.finance.amount(),
 			email: faker.internet.email(),
 			paymentMethod: faker.helpers.enumValue(PaymentMethod),
 			...params,
