aa82869ed5bfd320b76523455a62d29bdaade456
Diango Gavidia <diangogavidia@gmail.com>
2024-12-18 17:07:52 -0400

    chore: remove get method in UserFinder

--- src/modules/user/application/UserFinder.ts
+++ src/modules/user/application/UserFinder.ts
@@ -13,14 +13,4 @@ export class UserFinder {
 
 		return user.toJson();
 	}
-
-	async get(): Promise<unknown> {
-		const users = await this.repository.get();
-
-		if (!users) {
-			throw new NotFoundError(`Users not found`);
-		}
-
-		return users;
-	}
 }
