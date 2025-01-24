0563fb6ba6aa5214ad51730fd5d2c2e4677407e0
Diango Gavidia <diangogavidia@gmail.com>
2025-01-23 20:58:20 -0400

    refactor: comment rate limit

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,6 @@ import cors from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
 import Slack from "@slack/bolt";
 import { Elysia } from "elysia";
-import { rateLimit } from "elysia-rate-limit";
 
 import { SendWinnerEmailWhenRaffleIsDrawn } from "../modules/raffle/application/SendWinnerEmailWhenRaffleIsDrawn";
 import { RafflePostgresRepository } from "../modules/raffle/infrastructure/RafflePostgresRepository";
@@ -39,7 +38,7 @@ export class Server {
 		this.registerSubscribers();
 
 		this.app = new Elysia()
-			.use(rateLimit())
+			// .use(rateLimit())
 			.use(cors())
 			.use(
 				swagger({
